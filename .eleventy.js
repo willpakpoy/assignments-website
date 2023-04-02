555c45c2890603ff19465a97fe455373ed4179b2
Will Pak Poy <will@willpakpoy.com>
2023-04-02 19:56:13 +1000

    starting

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,16 @@
 // const pluginSass = require("eleventy-plugin-sass");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // eleventyConfig.addPassthroughCopy("static");
-  eleventyConfig.addWatchTarget("./styles/**/*");
   // eleventyConfig.addPlugin(pluginSass, {sourcemaps: true});
-
+  eleventyConfig.setBrowserSyncConfig({
+    files: "./_site/static",
+  });
   return {
     dir: {
       input: "assignments/work",
-      includes: "../_includes",
-      data: "../_data"
-    }
+      includes: "../../_includes",
+      data: "../../_data",
+    },
   };
-
 };
